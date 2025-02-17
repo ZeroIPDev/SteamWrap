ce23e13b42a1ca100b14d43474b5d103b7593b99
CCC200 <11540770+CCC200@users.noreply.github.com>
2025-02-17 13:45:05 -0500

    Rework GenerateItems, should function now

--- native/SteamWrap.cpp
+++ native/SteamWrap.cpp
@@ -2981,21 +2981,20 @@ DEFINE_PRIM(SteamWrap_GetResultItems,1);
 //-----------------------------------------------------------------------------------------------------------
 value SteamWrap_GenerateItems(value itemDefs, value itemAmts, value arrayLen)
 {
-	// CURRENTLY BROKEN! Do not use
-
-	// SteamInventoryResult_t _result;
-	// uint32 _len = val_int(arrayLen);
-	// SteamItemDef_t _items[_len];
-	// uint32 _amts[_len];
-	// int* _c_items = val_array_int(itemDefs);
-	// int* _c_amts = val_array_int(itemAmts);
-	// for(int i = 0; i < _len; i++) {
-	// 	_items[i] = _c_items[i];
-	// 	_amts[i] = _c_amts[i];
-	// }
-	// SteamInventory()->GenerateItems(&_result, _items, _amts, _len);
-	// return alloc_int(_result);
-	return alloc_int(-1);
+	SteamInventoryResult_t _result;
+	uint32 _len = val_int(arrayLen);
+	std::vector<SteamItemDef_t> _items;
+	std::vector<uint32> _amts;
+	_items.resize(_len);
+	_amts.resize(_len);
+	int* _c_items = val_array_int(itemDefs);
+	int* _c_amts = val_array_int(itemAmts);
+	for(int i = 0; i < _len; i++) {
+		_items[i] = _c_items[i];
+		_amts[i] = _c_amts[i];
+	}
+	SteamInventory()->GenerateItems(&_result, _items.data(), _amts.data(), _len);
+	return alloc_int(_result);
 }
 DEFINE_PRIM(SteamWrap_GenerateItems,3);
 
